e7ab74e354d97da7a9379eb0c52ead83fd57e82a
ignotur <ignotur@gmail.com>
2014-03-27 14:54:08 +0100

    Now I am going to maximise L for two Gaussian model by means of pikaia genetic algorithm. The new version of the Makefile allows us to build mixed .f and .cpp codes.

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 
 all: prepare_profiles.cpp libtc93.a analyse_model.cpp
-		g++ prepare_profiles.cpp -L. -ltc93 -L/opt/local/lib/ -lf95 -o prepare_profiles.out -Wall
-		g++ analyse_model.cpp -o analyse_model.out -Wall
+		g++ prepare_profiles.cpp -L. -ltc93 -L/opt/local/lib/ -lf95 -o prepare_profiles.out 
+		g++ analyse_model.cpp -c
+		gfortran -c pikaia.f
+		gfortran analyse_model.o pikaia.o -lc++ -lstdc++ -o analyse_model.out 
--- analyse_model.cpp
+++ analyse_model.cpp
@@ -5,12 +5,18 @@
 #include <cstdlib>
 #include <sstream>
 
+extern "C" float dwod_ (int *, float*);
+
 using namespace std;
 
 double const pi = 3.1415926;
 
+
 double model (double, double, double, double);
 double integ (double *, double, double, double);
+//float  dwod_ (int *, float *);
+
+/*
 
 int main (int argv, char * argc[]) {
 
@@ -87,7 +93,7 @@ w = 0.1*k3;
 }
 return 0;
 }
-
+*/
 //---------------------------------------------------------------
 // This is a function which models our distribution.
 // As in the original work by Brisken we use sum of two Gaussian
@@ -116,4 +122,80 @@ sum = 0;
 
 return sum;
 }
+
+
+float dwod_ (int * n, float * x) 	{
+float res;
+
+
+ifstream in;
+
+double static profile[100][1000];
+bool static flag = false;
+double entry_profile[1000], trash;
+double sigma_1, sigma_2, w;
+double L;
+
+stringstream name;
+string name_str;
+int static i=0, counter;
+
+	sigma_1 = x[0] * 1000.;
+	sigma_2 = x[1] * 1000.;
+	w = x[2];
+
+cout<< sigma_1<<"\t"<<sigma_2<<"\t"<<w<<endl;
+
+if (!flag)	{
+
+	cout<<"We are reading files..."<<endl;
+
+	do {
+		name << "/profiles/profile_"<<i<<".dat";
+		name_str = name.str();
+		char *basic_name = new char [name_str.size()];
+		memcpy(basic_name, name_str.c_str(), name_str.size());
+		name.clear();
+		name.str(string());
+		
+		cout<<"Opening file "<<	basic_name << endl;
+
+		in.open(basic_name);
+		if (in.good())		{  // if a file exists, read it.
+			counter = 11;
+			do {
+				in >> trash;
+				in >> profile[i][counter];
+				counter++;
+			} while (!in.eof());
+		}
+		else {
+			break;
+		}
+		in.close();
+		i++;
+	} while (1);
+i--;
+
+flag = true;
+
+}
+	// So, it is a right moment to start calculation of L from eq. (1)
+
+L = 1.;
+	for (int j=0; j < i; j++)	{
+		for (int k = 11; k < 1000; k++)
+			entry_profile[k] = profile[j][k];	
+		L *= integ (&entry_profile[0], sigma_1, sigma_2, w);
+
+	}
+L *= 1e110;
+
+cout<<"So, L is "<<L<<endl;
+
+res = L;
+
+return res;
+}
+
 
